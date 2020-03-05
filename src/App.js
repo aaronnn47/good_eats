fcc6e691e312f7a38fea73b33ec034ab94c5ed19
Aaron Kim <iamaaronkim@gmail.com>
2020-03-05 16:00:26 -0700

    added photos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import greens from './Images/greens.png'
+import bowl from './Images/bowl.png'
+import salad from './Images/salad.png'
 
 function App() {
   return (
     <div className="App">
-      <text>First design concepts</text>
+      <div className="text">
+       <text>First design concepts</text>
+
+      </div>
+      
+        <div className="photos">
+        <img src={greens} alt=''></img>
+        <img src={bowl} alt=''></img>
+        <img src={salad} alt=''></img>
+        </div>
+
     </div>
   );
 }
